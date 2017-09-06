cfb6862788eb152a88725fa1578d3399d1e3bae4
mk-pmb <mk-pmb@users.noreply.github.com>
2017-09-06 03:06:16 +0200

    v0.1.2

--- dry.js
+++ dry.js
@@ -6,6 +6,7 @@ var EX, keep = null, isAry = Array.isArray;
 
 function isStr(x, no) { return (((typeof x) === 'string') || no); }
 function fail(why) { throw new Error(why); }
+function countdown(n, f) { for (0; n > 0; n -= 1) { f(n); } }
 
 
 EX = function swell(reci) {
@@ -15,8 +16,10 @@ EX = function swell(reci) {
   }
   keep = {
     '#': 0,     // incrementing number
+    '®': 0,     // row counter
     '+': 0,     // increment distance
     '*': 1,     // repeats: how many times (total) to render this step
+    '*…': 1,    // default repeats: for this and the upcoming steps.
     '|':    null,   // row cells template
     '|fmt': null,   // post-format name
     '|cfg': null,   // custom config
@@ -36,14 +39,15 @@ function cfg(opt) { return (keep['|cfg'] || false)[opt]; }
 
 
 function split1ch(x) {
-  return ((x || false).split ? x.slice(1).split(new RegExp((x.match(/^\w/
-    ) ? '' : '\\') + x.substr(0, 1))) : x);
+  if (!x) { return x; }
+  if (!isStr(x)) { return x; }
+  if (x.match(/^\w/)) { return x.split(/\|/); }
+  return x.slice(1).split(new RegExp('\\' + x.substr(0, 1)));
 }
 
 
-function ifempty(x, d) {
+function ifset(x, d) {
   if (x) { return x; }
-  if (x === '') { return d; }
   if (x === null) { return d; }
   if (x === undefined) { return d; }
   return x;
@@ -59,7 +63,7 @@ function renderVars(s) {
   return s.replace(EX.varsRgx, renderVars.ins);
 }
 renderVars.ins = function (m, nameLong, nameChar) {
-  var n = (nameLong || nameChar), v = ifempty(keep[n], null);
+  var n = (nameLong || nameChar), v = ifset(keep[n], null);
   if (v === null) {
     if (cfg('undefVar') === 'orig') { return m; }
     fail('Cannot insert undefined variable: ' + m);
@@ -67,12 +71,16 @@ renderVars.ins = function (m, nameLong, nameChar) {
   return renderVars(v);
 };
 renderVars.cell = function (c, i) {
-  keep[','] = keep[i];
+  keep['©'] = (keep[i] || '');
   return renderVars(c);
 };
 
 
 function updateRowTpl(upd) {
+  if ((upd === null) || (upd === false)) {
+    keep['|'] = upd;
+    return;
+  }
   if (!upd) { return; }
   if (upd.substr(0, 1) === '$') { upd = renderVars(upd); }
   var tpl = (keep['|'] || []);
@@ -89,12 +97,15 @@ function updateRowTpl(upd) {
 }
 
 
-function postFormat(c, i) {
-  var n = postFormat.fmt[i], f;
-  if (!n) { return c; }
-  f = EX['fmt_' + n];
-  if (f) { return f(c); }
-  fail('unsupported post-format name: ' + n);
+function postFormat(cell, colIdx) {
+  var fmts = postFormat.fmt[colIdx];
+  if (!fmts) { return cell; }
+  fmts.split(/ /).forEach(function (fmt) {
+    var how = EX['fmt_' + fmt];
+    if (!how) { fail('unsupported post-format name: ' + fmt); }
+    cell = how(cell);
+  });
+  return cell;
 }
 
 
@@ -109,11 +120,12 @@ EX.recipeStep = function (step) {
   }
   if (isStr(step)) { step = [step]; }
   if (isAry(step)) { step = Object.assign({ '|': step[0] }, step.slice(1)); }
-  var repeat, rows = [], rendered, fmt = keep['|fmt'];
+  var rows = [], rendered, fmt = keep['|fmt'], repeat;
   Object.assign(keep, step,
     // protect special slots:
     { '|' : keep['|'] });
-  // Now we can calculate stuff that might want to renderVars():
+  // Now we can calculate stuff that might want to use vars:
+  repeat = ifset(step['*'], keep['*…']);
   updateRowTpl(step['|']);
   if (step['|fmt']) {
     fmt = split1ch(step['|fmt']);
@@ -121,24 +133,37 @@ EX.recipeStep = function (step) {
     postFormat.fmt = fmt;
   }
 
-  function renderRow() {
+  function renderRow(rpt) {
+    keep['*'] = rpt;
     keep['#'] += keep['+'];
     if (!keep['|']) { return; }
     //console.log(keep);
     rendered = keep['|'].map(renderVars.cell);
     //console.log(rowTpl, rendered);
     if (fmt) { rendered = rendered.map(postFormat); }
+    keep['®'] += 1;
     rows.push(rendered);
   }
 
-  for (repeat = (+keep['*'] || 0); repeat > 0; repeat -= 1) { renderRow(); }
+  if (repeat === +repeat) { countdown(repeat, renderRow); }
+  if (isAry(repeat)) { repeat.forEach(renderRow); }
   return rows;
 };
 
 
-EX.fmt_u_c = function (s) {
-  return s.trim().replace(/\s+/g, '_').toUpperCase();
-};
+function firstChar(f) {
+  return function (s) { return f(s.slice(0, 1)) + s.slice(1); };
+}
+
+
+function uc(s) { return s.toUpperCase(); }
+function lc(s) { return s.toUpperCase(); }
+EX.fmt_uc = uc;
+EX.fmt_uc1 = firstChar(uc);
+EX.fmt_u_c = function (s) { return uc(s.trim().replace(/\s+/g, '_')); };
+EX.fmt_lc = lc;
+EX.fmt_lc1 = firstChar(lc);
+EX.fmt_normsp = function (s) { return s.trim().replace(/\s+/g, ' '); };
 
 
 
